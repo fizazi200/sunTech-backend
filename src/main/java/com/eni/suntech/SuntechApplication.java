4417b0bdef706fe22793712fddc30a3bfba5b857
fizazi200 <fizazi2021@gmail.com>
2026-03-31 23:14:03 +0200

    init suntech backend

---
+++ src/main/java/com/eni/suntech/SuntechApplication.java
@@ -0,0 +1,13 @@
+package com.eni.suntech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuntechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuntechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eni/suntech/SuntechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eni.suntech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuntechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
